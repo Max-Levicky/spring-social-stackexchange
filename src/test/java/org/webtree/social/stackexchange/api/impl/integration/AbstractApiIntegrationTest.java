70421499bf6347fae43e9ea56dc6f4111fa10f8f
udjin <udjinskobelev@gmail.com>
2018-10-08 22:41:10 +0300

    #1 now token loads from environment

--- src/test/java/org/webtree/social/stackexchange/api/impl/integration/AbstractApiIntegrationTest.java
+++ src/test/java/org/webtree/social/stackexchange/api/impl/integration/AbstractApiIntegrationTest.java
@@ -15,8 +15,8 @@ public abstract class AbstractApiIntegrationTest {
 
     @Before
     public void setUp() throws Exception {
-        TOKEN = System.getProperty("token");
-        APP_KEY = System.getProperty("key");
+        TOKEN =  System.getenv("stackexchange.token");
+        APP_KEY = System.getenv("stackexchange.key");
         stackExchange = new StackExchangeTemplate(TOKEN, APP_KEY);
     }
 }
