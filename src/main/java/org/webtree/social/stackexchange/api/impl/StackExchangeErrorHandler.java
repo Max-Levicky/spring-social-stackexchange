3d5871cb150e61909ba636d0ee88949efb3b121c
udjin <udjinskobelev@gmail.com>
2018-10-15 17:03:16 +0300

    #1 ErrorHandler refactor ? change init of Map with error codes to guava

--- src/main/java/org/webtree/social/stackexchange/api/impl/StackExchangeErrorHandler.java
+++ src/main/java/org/webtree/social/stackexchange/api/impl/StackExchangeErrorHandler.java
@@ -3,6 +3,7 @@ package org.webtree.social.stackexchange.api.impl;
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import com.google.common.collect.ImmutableMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.client.ClientHttpResponse;
@@ -13,7 +14,6 @@ import org.webtree.social.stackexchange.api.ErrorHandler;
 import org.webtree.social.stackexchange.api.StackExchangeError;
 
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.Map;
 
 import static org.webtree.social.stackexchange.api.ErrorCodes.*;
@@ -26,48 +26,47 @@ public class StackExchangeErrorHandler extends DefaultResponseErrorHandler {
 
     private static final Logger logger = LoggerFactory.getLogger(StackExchangeErrorHandler.class);
     private static final String STACK_EXCHANGE_PROVIDER_ID = "stackExchange";
-    private Map<Integer, ErrorHandler> handlers = new HashMap<Integer, ErrorHandler>() {{
-        put(BAD_PARAMETER, (error) -> {
-            throw new UncategorizedApiException(STACK_EXCHANGE_PROVIDER_ID, error.getErrorMessage(), null);
-        });
-        put(ACCESS_TOKEN_REQUIRED, (error) -> {
-            throw new MissingAuthorizationException(STACK_EXCHANGE_PROVIDER_ID);
-        });
-        put(INVALID_ACCESS_TOKEN, (error) -> {
-            throw new InvalidAuthorizationException(STACK_EXCHANGE_PROVIDER_ID, error.getErrorMessage());
-        });
-        put(ACCESS_DENIED, (error) -> {
-            throw new InsufficientPermissionException(STACK_EXCHANGE_PROVIDER_ID);
-        });
-        put(NO_METHOD, (error) -> {
-            throw new ResourceNotFoundException(STACK_EXCHANGE_PROVIDER_ID, error.getErrorMessage());
-        });
-        put(KEY_REQUIRED, (error) -> {
-            throw new UncategorizedApiException(STACK_EXCHANGE_PROVIDER_ID, error.getErrorMessage(), null);
-        });
-        put(ACCESS_TOKEN_COMPROMISED, (error) -> {
-            throw new UncategorizedApiException(STACK_EXCHANGE_PROVIDER_ID, error.getErrorMessage(), null);
-        });
-        put(WRITE_FAILED, (error) -> {
-            throw new InternalServerErrorException(STACK_EXCHANGE_PROVIDER_ID, error.getErrorMessage());
-        });
-        put(DUPLICATE_REQUEST, (error) -> {
-            throw new UncategorizedApiException(STACK_EXCHANGE_PROVIDER_ID, error.getErrorMessage(), null);
-        });
-        put(INTERNAL_ERROR, (error) -> {
-            throw new InternalServerErrorException(STACK_EXCHANGE_PROVIDER_ID, error.getErrorMessage());
-        });
-        put(THROTTLE_VIOLATION, (error) -> {
-            throw new RateLimitExceededException(STACK_EXCHANGE_PROVIDER_ID);
-        });
-        put(EMPORARITY_UNAVAILABLE, (error) -> {
-            throw new ServerException(STACK_EXCHANGE_PROVIDER_ID, error.getErrorMessage());
-        });
-        put(UNKNOWN_ERROR, (error) -> {
-            throw new ApiException(STACK_EXCHANGE_PROVIDER_ID, error.getErrorMessage());
-        });
-    }};
+    private Map<Integer, ErrorHandler> handlers = ImmutableMap.<Integer, ErrorHandler>builder()
 
+            .put(BAD_PARAMETER, (error) -> {
+                throw new UncategorizedApiException(STACK_EXCHANGE_PROVIDER_ID, error.getErrorMessage(), null);
+            })
+            .put(ACCESS_TOKEN_REQUIRED, (error) -> {
+                throw new MissingAuthorizationException(STACK_EXCHANGE_PROVIDER_ID);
+            })
+            .put(INVALID_ACCESS_TOKEN, (error) -> {
+                throw new InvalidAuthorizationException(STACK_EXCHANGE_PROVIDER_ID, error.getErrorMessage());
+            })
+            .put(ACCESS_DENIED, (error) -> {
+                throw new InsufficientPermissionException(STACK_EXCHANGE_PROVIDER_ID);
+            })
+            .put(NO_METHOD, (error) -> {
+                throw new ResourceNotFoundException(STACK_EXCHANGE_PROVIDER_ID, error.getErrorMessage());
+            })
+            .put(KEY_REQUIRED, (error) -> {
+                throw new UncategorizedApiException(STACK_EXCHANGE_PROVIDER_ID, error.getErrorMessage(), null);
+            })
+            .put(ACCESS_TOKEN_COMPROMISED, (error) -> {
+                throw new UncategorizedApiException(STACK_EXCHANGE_PROVIDER_ID, error.getErrorMessage(), null);
+            })
+            .put(WRITE_FAILED, (error) -> {
+                throw new InternalServerErrorException(STACK_EXCHANGE_PROVIDER_ID, error.getErrorMessage());
+            })
+            .put(DUPLICATE_REQUEST, (error) -> {
+                throw new UncategorizedApiException(STACK_EXCHANGE_PROVIDER_ID, error.getErrorMessage(), null);
+            })
+            .put(INTERNAL_ERROR, (error) -> {
+                throw new InternalServerErrorException(STACK_EXCHANGE_PROVIDER_ID, error.getErrorMessage());
+            })
+            .put(THROTTLE_VIOLATION, (error) -> {
+                throw new RateLimitExceededException(STACK_EXCHANGE_PROVIDER_ID);
+            })
+            .put(EMPORARITY_UNAVAILABLE, (error) -> {
+                throw new ServerException(STACK_EXCHANGE_PROVIDER_ID, error.getErrorMessage());
+            })
+            .put(UNKNOWN_ERROR, (error) -> {
+                throw new ApiException(STACK_EXCHANGE_PROVIDER_ID, error.getErrorMessage());
+            }).build();
 
     StackExchangeErrorHandler() {
     }
